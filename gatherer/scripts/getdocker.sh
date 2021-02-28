9d61603371115ed8e4fd2057ecbda5a5fd62e105
James Campbell <jcampbell@anduril.com>
2021-02-28 00:13:36 -0500

    If it's hacky and you know it clap you hands (clap clap)!

--- gatherer/scripts/getdocker.sh
+++ gatherer/scripts/getdocker.sh
@@ -2,7 +2,7 @@ echo "Installing Docker CE..."
 sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum-config-manager --enable docker-ce-nightly
 sudo yum makecache fast
-mkdir docker
+mkdir -p docker
 cd docker
 echo "Getting all of the dependancies localized..."
 sudo yumdownloader --resolve docker-ce
