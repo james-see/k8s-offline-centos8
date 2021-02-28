6d47cbd703dded017af6d362308706e67d59f7b3
James Campbell <jcampbell@anduril.com>
2021-02-28 00:11:53 -0500

    Added another dependency

--- gatherer/scripts/getdocker.sh
+++ gatherer/scripts/getdocker.sh
@@ -1,1 +1,10 @@
-echo "made it here too"
+echo "Installing Docker CE..."
+sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --enable docker-ce-nightly
+sudo yum makecache fast
+mkdir docker
+cd docker
+echo "Getting all of the dependancies localized..."
+sudo yumdownloader --resolve docker-ce
+echo "Compressing the folder of docker stuff to a tarball..."
+tar cvzf ../docker.tar.gz *
