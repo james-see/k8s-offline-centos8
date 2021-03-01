f8bc66d1b9d9abe4fa2ed6e781c3c03a8a301ba0
James Campbell <jcampbell@anduril.com>
2021-02-28 21:32:53 -0500

    these confounded tests drive me nuts

--- gatherer/scripts/getdocker.sh
+++ gatherer/scripts/getdocker.sh
@@ -7,10 +7,10 @@ printf "${bldred} [%%] ${txtrst} Installing Docker CE local rpm to prep for tarb
 sudo yum-config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum-config-manager --enable docker-ce-nightly
 sudo yum makecache
-mkdir -p "$HOME"/docker
-cd "$HOME"/docker
+mkdir -p ${HOME}/docker
+cd ${HOME}/docker
 printf "${bldred} [%%] ${txtrst} Getting all of the dependancies localized...\n"
-sudo yumdownloader --resolve docker-ce
+sudo yumdownloader --resolve docker-ce --destdir ${HOME}/docker
 printf "${bldred} [%%] ${txtrst} Compressing the folder of docker stuff to a tarball and saves it to the user home directory as docker.tar.gz...\n"
-tar cvzf "$HOME"/docker.tar.gz *
+tar cvzf ${HOME}/docker.tar.gz *
 printf "${GREEN} [%%] ${txtrst} Docker prep complete. SCP docker.tar.gz to the offline server or manually transfer it over.\n"
