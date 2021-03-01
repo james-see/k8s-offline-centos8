a691b62af1dfd43b46210c9d55a2251fdcbd89dc
James Campbell <jcampbell@anduril.com>
2021-02-28 20:52:26 -0500

    should work I guess...

--- gatherer/scripts/checkos.sh
+++ gatherer/scripts/checkos.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 bldred='\033[1;31m'
 txtrst='\033[0m'
-
+GREEN='\033[0;32m'
 set +x
-printf "${bldred} [%%] ${txtrst} Checking for right Operating System...\n"
+printf "${bldred} [%%] ${txtrst} Checking for the right Operating System...\n"
 if [ "$(cat /etc/redhat-release | grep 'CentOS Linux release 8'  )" ]; then
-    echo "Found! CentOS 8 verified, continuing to preflight..."
+    printf "${GREEN} [%%] ${txtrst} Found! CentOS 8 verified, continuing to preflight..."
 else
-    echo "Not safe, not running on Centos 8, exiting and raising error."
+    printf "${bldred} [%%] ${txtrst} Not safe, not running on Centos 8, exiting and raising error."
     exit 1
 fi
--- gatherer/scripts/preflight.sh
+++ gatherer/scripts/preflight.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bldred='\033[1;31m'
 txtrst='\033[0m'
-
+GREEN='\033[0;32m'
 printf "${bldred} [%%] ${txtrst} Checking for epel-release...\n"
 sudo yum install -y epel-release.noarch yum-utils
-printf "${bldred} [%%] ${txtrst} Ready for Docker install...\n"
+printf "${GREEN} [%%] ${txtrst} Ready for Docker install...\n"
